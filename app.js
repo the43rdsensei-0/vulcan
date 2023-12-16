9af81057d7429922268f386f5ed2b9e50edcf088
the43rdsensei <ismailmuyideen.c05@gmail.com>
2023-12-16 22:42:37 +0100

    updated codes

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const slideList = [
+  { title: "ESSENTIAL", banner: "", caption: "Essential to infrastructure" },
+  { title: "INTEGRAL", banner: "", caption: "Integral to Development" },
+  { title: "INDISPENSABLE", banner: "", caption: "Indispensable to Our Way  of Life" },
+];
